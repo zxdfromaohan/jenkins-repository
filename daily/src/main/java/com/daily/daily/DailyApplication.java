d083f96485ad43ead624dcf4e613b37bfb84fc6e
zxd <729198387@qq.com>
2021-06-15 15:51:21 +0800

    aa

--- daily/src/main/java/com/daily/daily/DailyApplication.java
+++ daily/src/main/java/com/daily/daily/DailyApplication.java
@@ -3,11 +3,18 @@ package com.daily.daily;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 @MapperScan("com.daily.daily.yd.mapper")
-public class DailyApplication {
+public class DailyApplication  extends SpringBootServletInitializer{
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DailyApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(DailyApplication.class, args);
 	}
